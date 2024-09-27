1b6d5724e9f3ede647e948d2acc62eab2c215f49
Menard Sabugo <menardsabugo@gmail.com>
2024-09-27 12:44:57 +0800

    pseudocode

---
+++ exercise_3_diamond_shape.py
@@ -0,0 +1,6 @@
+# Added introduction header centered with dashes
+# Loop to repeatedly ask for an odd integer until the user provides one
+# Check if the number is even, prompting the user to try again
+# Displaying the upper portion of the diamond, top-middle
+# Displaying the lower portion of the diamond
+# Call the function to print the diamond
