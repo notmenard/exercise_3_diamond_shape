058633a3fcc8abfc8cbdc4884d0e23121bb5d4c8
Menard Sabugo <menardsabugo@gmail.com>
2024-09-27 12:48:10 +0800

    define the print_diamond function

--- exercise_3_diamond_shape.py
+++ exercise_3_diamond_shape.py
@@ -1,6 +1,8 @@
 # Added introduction header centered with dashes
 print(" Print-a-Diamond ".center(50, "-"))
 
+def print_diamond():
+    
 # Loop to repeatedly ask for an odd integer until the user provides one
 # Check if the number is even, prompting the user to try again
 # Displaying the upper portion of the diamond, top-middle
