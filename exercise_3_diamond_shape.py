2ed0b159fd545a33b507f1b5aa6a14c41136cb55
Menard Sabugo <menardsabugo@gmail.com>
2024-09-27 12:46:10 +0800

    adding intro line

--- exercise_3_diamond_shape.py
+++ exercise_3_diamond_shape.py
@@ -1,4 +1,6 @@
 # Added introduction header centered with dashes
+print(" Print-a-Diamond ".center(50, "-"))
+
 # Loop to repeatedly ask for an odd integer until the user provides one
 # Check if the number is even, prompting the user to try again
 # Displaying the upper portion of the diamond, top-middle
