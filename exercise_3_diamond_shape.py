08ec13c45c837fc6c511fb8c946a153328a4b2f5
Menard Sabugo <menardsabugo@gmail.com>
2024-09-27 12:51:52 +0800

    adding loop to ask for an odd integer

--- exercise_3_diamond_shape.py
+++ exercise_3_diamond_shape.py
@@ -2,9 +2,18 @@
 print(" Print-a-Diamond ".center(50, "-"))
 
 def print_diamond():
-    
-# Loop to repeatedly ask for an odd integer until the user provides one
-# Check if the number is even, prompting the user to try again
+
+    # Loop to repeatedly ask for an odd integer until the user provides one
+    while True:
+        n = int(input("\nEnter an odd integer: "))
+
+        # Check if the number is even, prompting the user to try again
+        if n % 2 == 0:
+            print("\nPlease provide an odd integer.")
+
+        else:
+            break
+
 # Displaying the upper portion of the diamond, top-middle
 # Displaying the lower portion of the diamond
 # Call the function to print the diamond
